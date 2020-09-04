4e8221ebda25e8c8125f060489641c44463e4438
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-09-04 16:50:13 +0100

    Added GSAP, adjusted project link, added CV

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-//Smooth scrolling
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+//Smooth scroll
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+//GSAP
+let socials = document.querySelectorAll(".social-links");
+let main = document.querySelectorAll(".main-links");
+let name = document.querySelectorAll(".name");
+
+const tl = new TimelineMax();
+tl
+    .fromTo(name, 1, { opacity: 0 }, { opacity: 1, ease: Power2.easeInOut })
+    .fromTo(main, 1, { x: "100vw", opacity: 0 }, { x: "0vw", opacity: 1, ease: Power2.easeInOut}, "-=0.8")
+    .fromTo(socials, 1, { x: "-100vw" , opacity: 0}, { x: "0vw", opacity: 1, ease: Power2.easeInOut }, "-=1")
+;
